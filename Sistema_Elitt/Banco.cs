3d96934cd4526bd68ddbf839c20c129a0f10fc4b
Luiz Felipe Cabral <83585742+LuizFCabral@users.noreply.github.com>
2021-12-29 16:30:48 -0300

    add tela de vendas

--- Sistema_Elitt/Banco.cs
+++ Sistema_Elitt/Banco.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +21,7 @@ namespace Sistema_Elitt
             {
                 if ((conexao == null) || (conexao.State != ConnectionState.Open))
                 {
-                    conexao = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=pedro5012;Database=LPA;");
+                    conexao = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=mariagorete;Database=ELITT;");
                     conexao.Open();
                 }
                 comando = new NpgsqlCommand();
