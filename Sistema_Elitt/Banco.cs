a4037f9a73901ff3d8cb1d59d7550fa34f2b27e9
Luiz Felipe Cabral <83585742+LuizFCabral@users.noreply.github.com>
2021-12-29 14:54:51 -0300

    criando classe banco e importando o pacote NuGet

---
+++ Sistema_Elitt/Banco.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Elitt
+{
+    public class Banco
+    {
+        public static NpgsqlConnection conexao;
+        public NpgsqlCommand comando;
+        public NpgsqlDataReader dreader;
+        public DataTable tabela;
+
+        public Banco()
+        {
+            try
+            {
+                if ((conexao == null) || (conexao.State != ConnectionState.Open))
+                {
+                    conexao = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=pedro5012;Database=LPA;");
+                    conexao.Open();
+                }
+                comando = new NpgsqlCommand();
+                comando.Connection = conexao;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro de conexão: " + ex.Message);
+            }
+        }
+    }
+}
